882f2e3c7899bbeeac15bf69ae48cf4a8b51cc49
Louise Rondia <louiserondia@gmail.com>
2023-09-02 15:24:39 +0200

    back to start

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
 
 	const srcs: string[] = ['/img/1.PNG', '/img/2.PNG', '/img/3.PNG'];
@@ -13,7 +14,7 @@
 	let ctx: CanvasRenderingContext2D;
 
 	onMount(() => {
-		if (typeof document !== 'undefined') {
+		if (typeof document !== 'undefined' && browser) {
 			canvas = document.createElement('canvas');
 			ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 		}
@@ -21,8 +22,7 @@
 
 	function randomIndex(index: number) {
 		let res = Math.floor(Math.random() * srcs.length);
-		while (res === index) 
-			res = Math.floor(Math.random() * srcs.length) ;
+		while (res === index) res = Math.floor(Math.random() * srcs.length);
 		return res;
 	}
 
@@ -50,15 +50,19 @@
 </script>
 
 {#if clicks < 3}
-<div class="img-container">
-	<img class="img-overlay" src={srcs[index]} alt={srcs[index]} />
-	<img
-	class="img-overlay"
-	src={objects[index]}
-	alt={objects[index]}
-	on:mousemove={(e) => handleMouseMove(e)}
-	on:mouseup={handleMouseUp}
-	/>
+	<div class="img-container">
+		<img class="img-overlay" src={srcs[index]} alt={srcs[index]} />
+		<img
+			class="img-overlay"
+			src={objects[index]}
+			alt={objects[index]}
+			on:mousemove={(e) => handleMouseMove(e)}
+			on:mouseup={handleMouseUp}
+		/>
+	</div>
+{:else}
+<div class="title">
+	<h1 on:mouseup={() => {clicks = 0;}}>SIMON JUNG</h1>
 </div>
 {/if}
 
@@ -75,6 +79,18 @@
 			margin: auto;
 			height: 80vh;
 			width: auto;
+		}
+	}
+	
+	.title {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		h1 {
+			cursor: pointer;
+			font-size: xx-large;
+			margin-bottom: 10rem;
 		}
 	}
 </style>
